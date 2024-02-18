42dfe6bd8f6416ce52ead1c1cb8eef7714930495
zhangwenjun <1346502061@qq.com>
2024-02-18 10:02:01 +0800

    Update:Result

--- src/Addon/Serve.rs
+++ src/Addon/Serve.rs
@@ -2,11 +2,11 @@ use std::thread;
 
 use napi::{
     threadsafe_function::{ErrorStrategy, ThreadsafeFunction, ThreadsafeFunctionCallMode},
-    Error, JsFunction,
+    JsFunction, Result,
 };
 
 #[napi(js_name = "CreateStaticFileServe")]
-pub fn CreateStaticFileServe(path: String, onOpen: JsFunction) -> Result<(), Error> {
+pub fn CreateStaticFileServe(path: String, onOpen: JsFunction) -> Result<()> {
     let tsfn: ThreadsafeFunction<(), ErrorStrategy::CalleeHandled> =
         onOpen.create_threadsafe_function(0, |_ctx| Ok(String::from("").into()))?;
     thread::Builder::new()
